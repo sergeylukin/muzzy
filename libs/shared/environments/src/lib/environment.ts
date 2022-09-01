89f52f2ba6f3e4fa6e78dd71e4127c75cfc89381
Sergey Lukin <contact@sergeylukin.com>
2022-09-01 07:13:41 +0300

    infra: set up shared environment lib
    CLI command:
    
    ```
    nx g @nrwl/workspace:lib environments --directory=shared \
    --tags="shared,constants"
    ```

---
+++ libs/shared/environments/src/lib/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// When building for production, this file is replaced with `environment.prod.ts`.
+
+export const environment = {
+  production: false,
+  api: {
+    host: 'localhost',
+    port: 3333,
+    hostport: 'http://localhost:3333',
+  },
+  front: {
+    host: 'localhost',
+    port: 4200,
+    hostport: 'http://localhost:4200',
+  },
+};
