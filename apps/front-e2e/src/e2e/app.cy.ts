05ac99a7ccf42179d8a1932509d51c5f6d1a6369
Sergey Lukin <contact@sergeylukin.com>
2022-09-01 05:08:07 +0300

    feat: display upload zone at /

--- apps/front-e2e/src/e2e/app.cy.ts
+++ apps/front-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,20 @@
-import { getGreeting } from '../support/app.po';
+import { getHeadingText } from '../support/app.po';
 
 describe('front', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display welcoming user action message', () => {
+    getHeadingText().contains('upload');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome front');
+  it('should display thank you message upon upload', () => {
+    // Drag & drop sample meme to the web page
+    cy.document().selectFile('src/fixtures/meme.jpeg', { action: 'drag-drop' });
+
+    // Verify we get a thank you
+    // @TODO
+    // Verify that there is a URL with 7 first characters of md5 hash of
+    // current timestamp (+/- 1 second)
+    getHeadingText().contains('Ur meme is up and happy!');
   });
 });
--- apps/front-e2e/src/support/app.po.ts
+++ apps/front-e2e/src/support/app.po.ts
@@ -1,1 +1,1 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeadingText = () => cy.get('h1');
