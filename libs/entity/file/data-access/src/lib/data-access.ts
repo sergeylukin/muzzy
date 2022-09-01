72fbe462fafca555b738e80778563446b85a5f57
Sergey Lukin <contact@sergeylukin.com>
2022-09-01 08:25:09 +0300

    feat: add data-access slice to File entity
    CLI command:
    
    ```
    nx g @nrwl/node:lib data-access --directory entity/file \
    --importPath @muzzy/file/data-access --simpleModuleName
    ```

---
+++ libs/entity/file/data-access/src/lib/data-access.ts
@@ -0,0 +1,18 @@
+import {
+  IFileUploadApiResponse,
+  FILE_UPLOAD_API_URL,
+} from '@muzzy/shared/api-interface';
+import { environment } from '@muzzy/shared/environments';
+
+export const fileUpload = (file: File): Promise<IFileUploadApiResponse> =>
+  fetch(environment.api.hostport + FILE_UPLOAD_API_URL, {
+    // Your POST endpoint
+    method: 'POST',
+    body: file, // This is your file object
+  })
+    .then(
+      (response) => response.json() // if the response is a JSON object
+    )
+    .catch(
+      (error) => console.log(error) // Handle the error response object
+    );
