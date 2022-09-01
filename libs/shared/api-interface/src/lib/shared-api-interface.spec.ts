3ece54f196a887ff28f4f6605a5a220a45978032
Sergey Lukin <contact@sergeylukin.com>
2022-09-01 06:12:55 +0300

    infra: setup shared api-interface lib
    CLI command:
    
    ```
    nx g @nrwl/workspace:lib api-interface --directory=shared \
    --tags="shared,constants"
    ```

---
+++ libs/shared/api-interface/src/lib/shared-api-interface.spec.ts
@@ -0,0 +1,7 @@
+import { sharedApiInterface } from './shared-api-interface';
+
+describe('sharedApiInterface', () => {
+  it('should work', () => {
+    expect(sharedApiInterface()).toEqual('shared-api-interface');
+  });
+});
---
+++ libs/shared/api-interface/src/lib/shared-api-interface.ts
@@ -0,0 +1,5 @@
+export interface IFileUploadApiResponse {
+  url: string;
+}
+
+export const FILE_UPLOAD_API_URL = '/v1/file';
