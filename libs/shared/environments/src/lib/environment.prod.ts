30d5fb091998daf60677c5f496c55abd064b01f1
Sergey Lukin <contact@sergeylukin.com>
2022-09-04 03:12:00 +0300

    fix: URLs across services in all environments, fix #3

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,24 +1,23 @@
 const productionDomain = process.env['NX_2ND_LEVEL_DOMAIN'];
-const isApi = process.env.RENDER_SERVICE_NAME === 'muzzy-api';
-const apiHost = isApi
-  ? process.env.RENDER_EXTERNAL_HOSTNAME
-  : process.env['NX_API_HOST'];
+const previewDomain = process.env['NX_PREVIEW_DOMAIN'];
+const MAIN_BRANCH_API_SERVICE_NAME =
+  process.env['NX_MAIN_BRANCH_API_SERVICE_NAME'];
+const isApi = process.env['NX_SERVICE_NAME'] === MAIN_BRANCH_API_SERVICE_NAME;
+const serviceHost = process.env['RENDER_EXTERNAL_HOSTNAME']?.replace(
+  new RegExp(`\.${productionDomain}|\.${previewDomain}`),
+  ''
+);
+const apiHost = isApi ? serviceHost : process.env['NX_API_HOST'];
+const isMainBranch = apiHost === MAIN_BRANCH_API_SERVICE_NAME;
 
-const apiBaseURL =
-  apiHost === 'muzzy-api'
-    ? `https://${apiHost}.${productionDomain}`
-    : `https://${apiHost}`;
-const apiPort = process.env.PORT;
-const frontHost = isApi
-  ? `${process.env.NX_FRONT_HOST}.${productionDomain}`
-  : process.env.RENDER_SERVICE_NAME;
-
-console.log('BUILD VARS');
-console.log(isApi);
-console.log(apiHost);
-console.log(process.env.HOST);
-console.log(apiPort);
-console.log(frontHost);
+const apiBaseURL = `https://${apiHost}.${
+  isMainBranch ? productionDomain : previewDomain
+}`;
+const apiPort = process.env['PORT'];
+const frontHost = isApi ? process.env['NX_FRONT_HOST'] : serviceHost;
+const frontBaseURL = `https://${frontHost}.${
+  isMainBranch ? productionDomain : previewDomain
+}`;
 
 export const environment = {
   production: true,
@@ -28,10 +27,10 @@ export const environment = {
     hostport: apiBaseURL,
   },
   front: {
-    host: frontHost || productionDomain,
-    hostport: `https://${frontHost}`,
+    host: frontHost,
+    hostport: frontBaseURL,
   },
   redis: {
-    url: process.env.REDIS_URL,
+    url: process.env['REDIS_URL'],
   },
 };
